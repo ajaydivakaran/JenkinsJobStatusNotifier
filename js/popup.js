536ee744c006cea6fd88a8d42ba5d87e95f89abb
Ajay <ajay_886@hotmail.com>
2014-05-01 00:10:21 +0530

    Add link to options page from popup

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,15 @@
 var popupViewModel = (function(){
+    var optionsLink;
+
+    function _optionsLinkClickHandler(event){
+        chrome.tabs.create({'url': chrome.extension.getURL("html/options.html")});
+        event.preventDefault();
+    }
 
     function initialize(){
+        optionsLink = $("#optionsPageLink");
+        optionsLink.on('click', _optionsLinkClickHandler);
+
         var backgroundPageWindow = chrome.extension.getBackgroundPage();
         backgroundPageWindow.jobStatusNotifier.getConfiguredJobStatuses(_renderJobStatuses);
     }
